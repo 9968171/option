a89d2c92ef880bd43189672fa2c384524c5bba18
9968171 <9968171@gmail.com>
2025-02-25 16:11:07 +0800

    Modify do.cpp

--- do.cpp
+++ do.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Water source collection allow other to never.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
